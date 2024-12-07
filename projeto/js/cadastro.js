9794c440194883a92b4e70b2680e7f97e36d859e
Flavio Silva <fal_silva@hotmail.com>
2024-12-07 16:41:08 -0300

    validação dos input do form

--- projeto/js/cadastro.js
+++ projeto/js/cadastro.js
@@ -1,7 +1,39 @@
+//Dados Iniciais
 const form = document.querySelector('#form')
 const nameInput = document.querySelector('#nome')
 const emailInput = document.querySelector('#email')
 const telefoneInput = document.querySelector('#fone')
 const apInput = document.querySelector('#ap')
 
-console.log(form, nameInput, emailInput)
+//Função
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    //verifica nome vazio
+    if(nameInput.value === ''){
+        alert('Preencha o nome!')        
+        return
+    } 
+
+    //verifica email é valido
+    if(emailInput.value === '' || !isEmailValid(emailInput.value)){
+        alert('Preencha com email válido!')        
+        return
+    }
+
+    //Todos os campos preenchidos, envia o form
+    form.submit()
+})
+
+//Função que valida email
+function isEmailValid(email){
+    //regex para validar email
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    )
+
+    if(emailRegex.test(email)){
+        return true
+    }
+    return false
+}
