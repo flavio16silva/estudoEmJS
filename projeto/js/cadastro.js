aa730ff63c97f151607a26fd3d3118990e429726
Flavio Silva <fal_silva@hotmail.com>
2024-12-07 15:17:12 -0300

    iniciando script de validação

---
+++ projeto/js/cadastro.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#form')
+const nameInput = document.querySelector('#nome')
+const emailInput = document.querySelector('#email')
+const telefoneInput = document.querySelector('#fone')
+const apInput = document.querySelector('#ap')
+
+console.log(form, nameInput, emailInput)
