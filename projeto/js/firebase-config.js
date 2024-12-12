308b57e2a25a8b8862bf36db56a9edfcd63b8323
Flavio Silva <fal_silva@hotmail.com>
2024-12-12 19:14:47 -0300

    arquivo firebase.js para uso no backEnd

---
+++ projeto/js/firebase-config.js
@@ -0,0 +1,18 @@
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+
+// Sua configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBUBpOHK7rTVj7V5KxH0m8ApA9wxRMxoyk",
+  authDomain: "edificio-back01.firebaseapp.com",
+  projectId: "edificio-back01",
+  storageBucket: "edificio-back01.firebasestorage.app",
+  messagingSenderId: "657186507275",
+  appId: "1:657186507275:web:6d3881c1d7f7a305d6550e",
+  measurementId: "G-FR66PXJY5F"
+};
+
+// Inicializando o Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
