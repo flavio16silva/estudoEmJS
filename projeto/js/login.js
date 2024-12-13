5b8b802b7a30cfa5dec2ca7ad4435051b32d7e63
Flavio Silva <fal_silva@hotmail.com>
2024-12-13 10:48:43 -0300

    criação script para autenticar usuario cadastrado no Firebase

--- projeto/js/login.js
+++ projeto/js/login.js
@@ -1,2 +1,30 @@
 //Importações
 import app from './firebase-config'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
+const auth = getAuth(app)
+const formLogin = document.querySelector('#loginForm')
+
+if(formLogin){
+    formLogin.addEventListener('submit', (e)=> {
+        e.preventDefault()                     //evita o recarregamento da pagina
+        
+        //Valores dos campos
+        const formEmail = document.querySelector('#floatingInputEmail')
+        const formPass = document.querySelector('#floatingInputPass')
+
+        //Autenticar no Firebase
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                //Login correto
+                const user = userCredential.user
+                console.log('Usuario logado: ', user)
+
+                //Direcionando pagina da ATA
+                window.location.href = 'projeto.html'
+            })
+            .catch((error) => {
+                console.error('Erro ao fazer login: ', error)
+            })       
+    })
+}
