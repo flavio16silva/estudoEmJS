d78d8455d8d442c0c2c42f5e6549d1031d1eb6a6
Flavio Silva <fal_silva@hotmail.com>
2024-12-17 22:35:48 -0300

    criando função de validação

--- projeto/js/projeto.js
+++ projeto/js/projeto.js
@@ -18,19 +18,29 @@ const errors = []
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    
+    if (ataInput.value.trim() === ''){
+        showError(ataInput, 'Preencha o título da Ata')
+    } 
+   
+
+    if(horasT.value <= horasI.value){
+        event.preventDefault()
+        alert('A hora de início deve ser anterior à hora de término')
+    }
+
+  
+
     const errors = [];
     //trim() - Remove espaços em branco no início e no final do valor 
-    if (ataInput.value.trim() === '') errors.push('Preencha o título da Ata');
-    if (dataInput.value.trim() === '') errors.push('Preencha a data da Ata');
-    if (horasI.value.trim() === '') errors.push('Preencha o horário de início');
-    if (horasT.value.trim() === '') errors.push('Preencha o horário de término');
-    if (localInput.value.trim() === '') errors.push('Preencha o local da reunião');
-    if (presidInput.value.trim() === '') errors.push('Preencha o nome do Presidente');
-    if (secreInput.value.trim() === '') errors.push('Preencha o nome do Secretário');
-    if (participaInput.value.trim() === '') errors.push('Preencha os Participantes');
-    if (discussoesInput.value.trim() === '') errors.push('Preencha os relatos das Discussões');
-    if (obsInput.value.trim() === '') errors.push('Preencha as Observações');
+    // if (dataInput.value.trim() === '') errors.push('Preencha a data da Ata');
+    // if (horasI.value.trim() === '') errors.push('Preencha o horário de início');
+    // if (horasT.value.trim() === '') errors.push('Preencha o horário de término');
+    // if (localInput.value.trim() === '') errors.push('Preencha o local da reunião');
+    // if (presidInput.value.trim() === '') errors.push('Preencha o nome do Presidente');
+    // if (secreInput.value.trim() === '') errors.push('Preencha o nome do Secretário');
+    // if (participaInput.value.trim() === '') errors.push('Preencha os Participantes');
+    // if (discussoesInput.value.trim() === '') errors.push('Preencha os relatos das Discussões');
+    // if (obsInput.value.trim() === '') errors.push('Preencha as Observações');
 
     //Se houver erros - campo obrigatorio não preenchido
     if (errors.length > 0) {
@@ -38,5 +48,15 @@ form.addEventListener('submit', (event) => {
         return;
     }
 
+
     form.submit()
 })
+
+//Exibir erro
+function showError(Input, message) {
+    const errorElement = document.createElement('small')
+    errorElement.className = 'text-danger'
+    errorElement.innerText = message
+    Input.parentElement.appendChild(errorElement)
+}
+
